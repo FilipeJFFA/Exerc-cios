9de6d97beea1bfcdbaac0cf95c49d680189d9593
FilipeJFFA <152027664+FilipeJFFA@users.noreply.github.com>
2023-11-30 11:50:28 +0000

    Update main.c

--- Ficha 6/Ex 8/main.c
+++ Ficha 6/Ex 8/main.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 
-void mostraFicheiro(char nome) {
-    FILE *arquivo = fopen(nome  , "r");
+void mostraFicheiro(char *nome) {
+    FILE *arquivo = fopen(nome, "r");
 
     if (arquivo == NULL) {
         perror("Erro ao abrir o arquivo");
         return;
     }
 
-    char linha[1000];  
-    char linhaMaisLonga[1000] = "";  
+    char linha[1000];
+    char linhaMaisLonga[1000] = "";
     int tamanho1 = 0;
-    int tamanho2= strlen(linha);
-    int i;
+    int tamanho2;
+    tamanho2=strlen(linha);
 
-    for (int i = 0; i < strlen(linha); i++) {
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
 
         if (tamanho2 > tamanho1) {
-            tamanho2 = tamanho1;
-            printf("%s", linha);
+            tamanho1 = tamanho2;
+            printf("%s",linha);
         }
     }
 
@@ -34,6 +34,11 @@ int main() {
     printf("Introduza o nome do arquivo: ");
     gets(nome);
     
+    mostraFicheiro(nome);
+
+    return 0;
+}
+
 
     mostraFicheiro(nome);
 
